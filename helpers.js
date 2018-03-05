b1ec1b5606f4e714cdf306f8f95b69624a286c63
Mcdavid95 <mcdavidemereuwa95@gmail.com>
2018-03-05 09:25:15 +0100

    push

--- helpers.js
+++ helpers.js
@@ -93,6 +93,7 @@ export const handleFeedback = (message) => {
          axios.get('http://api.football-data.org/v1/competitions/445/leagueTable')
               .then((response) => {
                   standings = JSON.parse(response.data).standing.slice(0, 4);
+                  console.log("I got here first", standings)
                       return {
                           "attachment": {
                               "type": "template",
@@ -103,7 +104,6 @@ export const handleFeedback = (message) => {
                               }
                           }
                       };
-                      console.log("I got here first", standings)
               })
               .catch((error) => {
                   return { error }
